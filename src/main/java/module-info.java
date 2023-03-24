eabb2c8826e9a4e3d9cd8e4bb6e02d718ca079ae
Andreas200536996 <113151311+Andreas200536996@users.noreply.github.com>
2023-03-23 22:10:45 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp1008st200536996assignment2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.comp1008st200536996assignment2 to javafx.fxml;
+    exports com.example.comp1008st200536996assignment2;
+}
