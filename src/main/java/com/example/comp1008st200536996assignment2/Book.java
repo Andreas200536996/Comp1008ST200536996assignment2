9b0521d0b8726934bb072d2dac3b123a885f62d9
Andreas200536996 <113151311+Andreas200536996@users.noreply.github.com>
2023-04-01 20:49:39 -0400

    Added methods to classes

--- src/main/java/com/example/comp1008st200536996assignment2/Book.java
+++ src/main/java/com/example/comp1008st200536996assignment2/Book.java
@@ -7,6 +7,44 @@ public class Book {
     private double price;
     private String author;
     private Image image;
+
+
+    public Book(String title, double price, String author, Image image) {
+        setTitle(title);
+        setPrice(price);
+        setAuthor(author);
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public Image getImage() {
+        String imageLink = "images/" + title + ".jpg";
+        return new Image(Book.class.getResourceAsStream(imageLink));
+    }
 }
+
 
 
--- src/main/java/com/example/comp1008st200536996assignment2/Library.java
+++ src/main/java/com/example/comp1008st200536996assignment2/Library.java
@@ -1,4 +1,33 @@
 package com.example.comp1008st200536996assignment2;
 
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> bookShelf;
+
+    public Library() {
+        this.bookShelf = new ArrayList<>();
+    }
+
+    public ArrayList<Book> addBookToShelf(Book book) {
+        bookShelf.add(book);
+        return bookShelf;
+    }
+
+    public ArrayList<Book> getBookShelf() {
+        return bookShelf;
+    }
+
+    public double getTotalValueOfLibrary() {
+        double totalValue = 0;
+        for (int i=0; i<=bookShelf.size(); i++ ) {
+            totalValue += bookShelf.get(i).getPrice();
+        }
+        return totalValue;
+    }
+
+    public int getSizeOfLibrary() {
+        return bookShelf.size();
+    }
 }
